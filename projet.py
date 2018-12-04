56851aafde1545fe320986bcb981e0926171d85e
Chaferfu <mathba@gmx.com>
2018-12-04 14:41:02 +0100

    On a bcp avancé

--- projet.py
+++ projet.py
@@ -1,21 +1,84 @@
 #projet de nathan et mathias
 # coding: utf-8
 
+from __future__ import print_function
 import numpy as np
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
 from sklearn import svm
+from math import sqrt
+from sklearn.neighbors import KNeighborsClassifier
 
+
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+
+def normalize(v):
+    norm=np.linalg.norm(v, ord=1)
+    if norm==0:
+        norm=np.finfo(v.dtype).eps
+    return v/norm
 
 def importData():
 	train_data = loadmat("train_32x32.mat")
 	test_data = loadmat("test_32x32.mat")
 	return train_data, test_data;
 
+def fondBlanc(img):
+	av = np.average(img)
+	if av < 106 :
+		return 255 - img
+	else:
+		return img
+
 def contraste(img):
 	
-	img[img>155] = 255
-	img[img<=20] = 0
+	img[img>240] = 255
+	img[img<=100] = 0
+	return img
+
+def CDM_predict(img, prototypes):
+	label = 0 
+	scores = []
+	img = contraste(fondBlanc(rgb2gray(img)))
+	img = img.flatten()
+	for i in range(len(prototypes)):
+		diff = img - prototypes[i] 
+		scores.append(sqrt(np.dot(diff,diff)))
+
+	return scores.index(min(scores))
+
+def pretraitement(data):
+	print("Application du pretraitement")
+	print(data.shape)
+	print(len(data))
+	imagesRetouchees = []
+	for i in range(data.shape[3]):
+		#print("ca travaille ", i, data.shape[3], end = '')
+		if i % data.shape[3]/100 == 0:
+			print("|", end = '')
+		imagesRetouchees.append( contraste(fondBlanc(rgb2gray(data[:, :, :, i]))))
+	
+	#imagesRetouchees = np.array(imagesRetouchees)
+	print("")
+	return np.array(imagesRetouchees)
+
+
+def preparerPourFit(data):
+	print("Preparation des donnees pour fit le modele")
+	#data = train_data['data']
+	#data = np.transpose(data, (3,0,1,2))
+	print(data.shape , end='')
+	#print(data)
+	nbElt = data.shape[0]
+	nbFeatures = data.shape[1]*data.shape[2]
+	data = data.reshape(nbElt, nbFeatures)
+
+	print(" -> ", data.shape)
+
+	return data
+
+
 
 def CDM():
 
@@ -25,14 +88,18 @@ def CDM():
 
 	train_data, test_data = importData()
 
+	imagesRetouchees = []
+
+	for i in range(len(train_data['y'])):
+		imagesRetouchees.append(contraste(fondBlanc(rgb2gray(train_data['X'][:, :, :, i]))))
 
 	print("tri des images")
 
 	for i, data in enumerate(train_data['y']):
 		if data[0] == 10:
-			populations[0].append(train_data['X'][:, :, :, i])
+			populations[0].append(imagesRetouchees[i])
 		else:
-			populations[data[0]].append(train_data['X'][:, :, :, i])
+			populations[data[0]].append(imagesRetouchees[i])
 
 
 	print("Calcul des representants")
@@ -42,22 +109,61 @@ def CDM():
 
 		populations[i] = np.array(populations[i])
 		prototypes.append(np.average(populations[i], axis = 0))
-
-
-
-
-
-
-	# contraste(train_data["X"])
-	# image_idx = 0
-	# print("Label:", train_data["y"][image_idx])
-	# print(train_data["X"][:, :, :, image_idx])
-	plt.imshow(prototypes[1])
-	plt.show()
-
+		prototypes[i] = prototypes[i].flatten()
+		
+	nbTrouve = 0
+	for i, data in enumerate(test_data['y']):
+		label = CDM_predict(test_data['X'][:, :, :, i], prototypes)
+		print(data[0], label)
+		if data[0] == 10 and label == 0:
+			nbTrouve += 1
+		elif data[0] == label:
+			nbTrouve += 1
+			
+	print("resultttttttttttttttat", float(nbTrouve)/float(len(test_data['y'])))
 	return
 
+def KNN():
+
+	train_data, test_data = importData()
+
+	imagesRetouchees = pretraitement(train_data["X"])
+	X = preparerPourFit(imagesRetouchees)
+
+	y = np.array([])
+	for truc in train_data['y']:
+		y = np.append(y, (truc[0]))
+
+	neigh = KNeighborsClassifier(n_neighbors=3)
+
+	print("Entrainement de neigh")
+	neigh.fit(X, y)
+
+	Xtest = preparerPourFit(pretraitement(test_data["X"]))
+
+	nbReussites = 0
+	print("Neigh is predicting, please wait")
+	reponses = neigh.predict(Xtest[:1000])
+
+	print("Alors maintenant Neigh, on va te poser " + str(len(reponses)) + " questions ok ? ")
+
+	for i, reponse in enumerate(reponses):
+		print(int(reponse), test_data['y'][i][0])
+		if int(reponse) == test_data['y'][i][0]:
+			nbReussites+=1
+
+	print("Neigh a trouvé " + str(nbReussites) + " reponses justses. Bravo Neigh !")
+	print("pourcentage de réussite de Neigh : " + str((float(nbReussites)/float(len(reponses)))))
+	print("au revoir")
+
+
+
+
+
+
 def SVM():
+
+
 	train_data, test_data = importData()
 
 	X = train_data['X']
@@ -65,35 +171,49 @@ def SVM():
 	nbElt = X.shape[0]
 	nbFeatures = X.shape[1]*X.shape[2]*X.shape[3]
 	X = X.reshape(nbElt, nbFeatures)
+	print("normalisation !!")
+	for i in range(len(X)):
+		X[i] = normalize(X[i])
+		if i % (len(X)/10):
+			print("|", end="")
+
+
+	print(X[10])
 
 	y = np.array([])
 	for truc in train_data['y']:
 		y = np.append(y, (truc[0]))
 	
-	print(X.shape)
+	print("shape x", X.shape)
 	print(y.shape)
 	print("Creation Bob")
-	Bob = svm.SVC(verbose = True)
+	Bob = svm.SVC()
 	print("OKI, début du classifieur, on s'entraine hop hop hop")
-	Bob.fit(X[:100], y[:100])
+	Bob.fit(X, y)
 
-	print("Alors maintenant Bob, on va te poser " + str(len(test_data)) + " questions ok ? ")
 
 	Xtest = test_data['X']
 	Xtest = np.transpose(Xtest, (3,0,1,2))
 	nbElt = Xtest.shape[0]
+	print(nbElt, "C'etrdgklsfgd")
 	nbFeatures = Xtest.shape[1]*Xtest.shape[2]*Xtest.shape[3]
 	Xtest = Xtest.reshape(nbElt, nbFeatures)
 
 	nbReussites = 0
-	reponses = Bob.predict(Xtest)
+	print("Bob is predicting, please wait")
+	reponses = Bob.predict(Xtest[:100])
+
+	print("Alors maintenant Bob, on va te poser " + str(nbElt) + " questions ok ? ")
+
 	for i, reponse in enumerate(reponses):
 		print(int(reponse), test_data['y'][i][0])
 		if int(reponse) == test_data['y'][i][0]:
 			nbReussites+=1
 
 	print("Bob a trouvé " + str(nbReussites) + " reponses justses. Bravo Bob !")
-	print("pourcentage de réussite de Bob : " + str((nbReussites/len(reponses))))
+	print("pourcentage de réussite de Bob : " + str((float(nbReussites)/float(len(reponses)))))
 	print("au revoir")
 
-SVM()
+#SVM()
+#CDM()
+KNN()
