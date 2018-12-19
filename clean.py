4a24da31441fff13f745675027a38be250d20592
Chaferfu <mathba@gmx.com>
2018-12-19 19:39:50 +0100

    bite

--- clean.py
+++ clean.py
@@ -7,6 +7,7 @@ import time
 import numpy as np
 from scipy.io import loadmat
 from sklearn import svm
+from sklearn.decomposition import PCA
 from math import sqrt
 from sklearn.neighbors import KNeighborsClassifier
 
@@ -41,9 +42,13 @@ def pretraitement(data, val = None, upper = None, lower = None, blackWhite = Non
 
 	traitees = []
 
-	for i in range(len(data)):
+	data = np.transpose(data, (3,0,1,2))
 
-		traite = data[:,:,:,i]
+	nbImages = len(data)
+
+	for i in range(nbImages):
+
+		traite = data[i,:,:,:]
 
 		if blackWhite:
 			traite = rgb2gray(traite)
@@ -56,13 +61,89 @@ def pretraitement(data, val = None, upper = None, lower = None, blackWhite = Non
 
 		traitees.append(traite)
 
+	traitees = np.array(traitees)
+	
+	if blackWhite:
+		traitees = traitees.reshape(nbImages, 32*32)
+	else:
+		traitees = traitees.reshape(nbImages, 32*32*3)
+
 	return np.array(traitees);
 
+def acp(data, nc):
+
+	pca = PCA(n_components=nc)
+	data = pca.fit_transform(data)
+
+	return data
+
+def CDM_predict(img, prototypes):
+
+	label = 0 
+	scores = []
+	
+	for i in range(len(prototypes)):
+		diff = img - prototypes[i] 
+		scores.append(sqrt(np.dot(diff,diff)))
+
+	return scores.index(min(scores))
+
+def CDM(train_imgs, train_labels, test_imgs, test_labels):
+
+	populations = [[],[],[],[],[],[],[],[],[],[]]
+
+	prototypes = []
+
+	print("tri des images")
+	startFit = time.clock()
+
+	for i, data in enumerate(train_labels):
+		if data[0] == 10:
+			populations[0].append(train_imgs[i])
+		else:
+			populations[data[0]].append(train_imgs[i])
+
+
+	print("Calcul des representants")
+	for i in range(10):
+		print("Calcul du representant de " + str(i) + " ")
+
+		populations[i] = np.array(populations[i])
+		prototypes.append(np.average(populations[i], axis = 0))
+		prototypes[i] = prototypes[i].flatten()
+
+	fitTime = str(time.clock() - startFit)
+
+	print("prediction")
+	startPredict = time.clock()
+	nbTrouve = 0
+	for i, data in enumerate(test_labels):
+		label = CDM_predict(test_imgs[i], prototypes)
+		if data[0] == 10 and label == 0:
+			nbTrouve += 1
+		elif data[0] == label:
+			nbTrouve += 1
+
+	predictTime = str(time.clock() - startPredict)
+			
+	reussite = str(float(nbTrouve)/float(len(test_labels)))
+	print("reussitettttttttttttttat" + reussite)
+
+	return [str(datetime.datetime.now()), reussite, fitTime, predictTime]
 
 
 train_data, test_data = importData()
 
-traitees = pretraitement(train_data["X"], val = 10)
+traitees = pretraitement(train_data["X"], val = 106, upper = 240, lower = 100, blackWhite = True)
 
-print(traitees)
+print(train_data["X"].shape)
+# print(traitees)
 print(traitees.shape)
+
+
+traitessTest = pretraitement(test_data["X"], val = 106, upper = 240, lower = 100, blackWhite = True)
+
+traitees = acp(traitees, 500)
+traitessTest = acp(traitessTest, 500)
+
+CDM(traitees, train_data['y'], traitessTest, test_data['y'])
